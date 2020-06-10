f8c16eb5b7e15532f4fcf5f862c498bcbbeb2c01
TPettijohn <50237844+TPettijohn@users.noreply.github.com>
2020-06-10 11:07:52 -0500

    0.04 Upload
    0.04 - 
    Bugfix: Start-Sleep timer used between backups was still set to 1 for testing, causing backups to be created every second. Functional, but excessive; Changed back to 60*15 (15 minutes)
    Documentation: Added Change Log as a comment block to the script. 
    Optimization: Added some Write-Host's to Restore and Cleanup sections to clarify what's running.

--- BL3SaveGameBackerUpper.ps1
+++ BL3SaveGameBackerUpper.ps1
@@ -1,8 +1,25 @@
-﻿# Version 0.03
+﻿# Version 0.04
 # Created by T. Pettijohn on 2020-06-09
 # Modified by T. Pettijohn on 2020-06-10
 # GNU GPL v2
 
+# Change Log:
+<#
+    0.01 - Initial build for testing
+    
+    0.02 - Bugfix: "...\Saved\Save Games" changed to "...\Saved\SaveGames" to fix error message. 
+    
+    0.03 - Feature Add: Cleanup selection and logic to remove all but the newest 10 ZIP archives (equivalent to 2.5 hours of game time).
+    
+    0.04 - Bugfix: Start-Sleep timer used between backups was still set to 1 for testing, causing backups to be created every second. Functional, but excessive; Changed back to 60*15 (15 minutes)
+           Documentation: Added Change Log as a comment block to the script. 
+           Optimization: Added some Write-Host's to Restore and Cleanup sections to clarify what's running.
+
+#>
+
+
+
+# Start of Script
 
 # Logic for creating backup names and creating the backup archive
 function Backup-SaveGames {
@@ -48,17 +65,20 @@ Clear-Host
 # Prompt user for input
 $Choice = (Read-Host "Do you want to Back Up, Restore, or Clean Up? (B/R/C)")
 
+Clear-Host
+
 # Backup Section
 If ($Choice -ieq "B"){ 
-Write-Host ("Running the Borderlands 3 Save Game Backer Upper. Press Ctrl + C to quit.")
+    Write-Host ("Running the Borderlands 3 Save Game Backer Upper. Press Ctrl + C to quit. `n")
     While($True){
         Backup-SaveGames
-        Start-Sleep -S (1)
+        Start-Sleep -S (60*15)
     }
 }
 
 # Restore Section
 ElseIf ($Choice -ieq "R"){
+    Write-Host ("Running the Borderlands 3 Save Game Fixer Upper. `n")
     $RestoreChoice = (Read-Host "OK to restore the most recent backup? (Y/N)")
     If ($RestoreChoice -ieq "Y"){
         Restore-SaveGames
@@ -70,6 +90,7 @@ ElseIf ($Choice -ieq "R"){
 
 
 ElseIf ($Choice -ieq "C"){
+    Write-Host ("Running the Borderlands 3 Save Game Cleaner Upper. `n")
     $CleanChoice = (Read-Host "OK to delete all except the newest 10 (Equiv. 2.5 Hours) backups? (Y/N)")
     If ($CleanChoice -ieq "Y"){
         Get-ChildItem -Path $BackupLoc | # Checks all files in Backup directory
